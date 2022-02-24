07e5a5a584b464d21c984151631663dd7a22022d
bk75R <82215025+bk75R@users.noreply.github.com>
2022-02-24 17:03:23 +0000

    Update NRSDeaths_InquiryGraphs.R
    Calculated cumulative 2022 excess deaths. Modified "Cumulative Excess Deaths in Scotland, 2020 and 2021 (faceted by age group)" graph to show "2022" data.

--- NRSDeaths_InquiryGraphs.R
+++ NRSDeaths_InquiryGraphs.R
@@ -3,14 +3,6 @@ setwd(GraphsDirectory)
 # Add cumulative deaths to DeathsTogether df
 # May need a different data frame as this doesn't include COVID/non-COVID split. Just all causes & expected.
 
-DeathsTogetherCum2021 <- DeathsTogether %>%
-  filter(Date >= as.Date("2021-01-01")) %>%
-  filter(Cause == "All") %>%
-  group_by(Age) %>%
-  mutate(cumDeaths = cumsum(Deaths)) %>%
-  mutate(cumExcess = cumsum(Excess))
-  
-
 DeathsTogetherCum2020 <- DeathsTogether %>%
   filter(Date >= as.Date("2019-12-01") & Date <= as.Date("2020-12-31")) %>%
   filter(Cause == "All") %>%
@@ -18,10 +10,24 @@ DeathsTogetherCum2020 <- DeathsTogether %>%
   mutate(cumDeaths = cumsum(Deaths)) %>%
   mutate(cumExcess = cumsum(Excess))
 
-DeathsTogetherCum2020_2021 <- rbind.data.frame(DeathsTogetherCum2020,DeathsTogetherCum2021)
+DeathsTogetherCum2021 <- DeathsTogether %>%
+  filter(Date >= as.Date("2021-01-01") & Date <= as.Date("2021-12-31")) %>%
+  filter(Cause == "All") %>%
+  group_by(Age) %>%
+  mutate(cumDeaths = cumsum(Deaths)) %>%
+  mutate(cumExcess = cumsum(Excess))
+
+DeathsTogetherCum2022 <- DeathsTogether %>%
+  filter(Date >= as.Date("2022-01-01") & Date <= as.Date("2022-12-31")) %>%
+  filter(Cause == "All") %>%
+  group_by(Age) %>%
+  mutate(cumDeaths = cumsum(Deaths)) %>%
+  mutate(cumExcess = cumsum(Excess))
+
+DeathsTogetherCum2020_2022 <- rbind.data.frame(DeathsTogetherCum2020,DeathsTogetherCum2021,DeathsTogetherCum2022)
 
 # Add year, ISO week and gradient of cumExcess and cumDeaths.
-DeathsTogetherCum2020_2021 <- DeathsTogetherCum2020_2021 %>%
+DeathsTogetherCum2020_2022 <- DeathsTogetherCum2020_2022 %>%
   group_by(Age) %>%
   mutate(Year = as.factor(isoyear(Date))) %>%
   mutate(WeekNo = isoweek(Date)) %>%
@@ -37,17 +43,19 @@ DeathsTogetherCum2020_2021 <- DeathsTogetherCum2020_2021 %>%
 #
 ###############################################################################
 
-YearColours = c("2020" = "grey50",
-                "2021" = "black")
-YearLineThicknesses = c("2020" = 0.5,
-                        "2021" = 1.5)
+YearColours = c("2020" = "grey25",
+                "2021" = "grey50",
+                "2022" = "black")
+YearLineThicknesses = c("2020" = 0.25,
+                        "2021" = 1,
+                        "2022" = 1)
 
 GraphCaptionOld <- GraphCaption
 GraphCaption <- "Data source: https://www.nrscotland.gov.uk/statistics-and-data/statistics/statistics-by-theme/vital-events/general-publications/\nweekly-and-monthly-data-on-births-and-deaths/deaths-involving-coronavirus-covid-19-in-scotland/related-statistics"
 
-GraphFileName = " Cumulative Excess Deaths in Scotland, 2020 and 2021 (faceted by age group).png"
+GraphFileName = " Cumulative Excess Deaths in Scotland, 2020 and 2022 (faceted by age group).png"
 
-NRSWeeklyDeaths_Excess_Graph = ggplot(data = DeathsTogetherCum2020_2021,
+NRSWeeklyDeaths_Excess_Graph = ggplot(data = DeathsTogetherCum2020_2022,
                                       mapping = aes(x=WeekNo,
                                                     y=cumExcess,
                                                     colour = Year,
@@ -69,7 +77,7 @@ NRSWeeklyDeaths_Excess_Graph = ggplot(data = DeathsTogetherCum2020_2021,
                      breaks = c(0,10,20,30,40,50))+
   scale_y_continuous(name = "Cumulative Excess Deaths",
                      labels = scales::comma)+
-  ggtitle("Cumulative Excess Deaths in Scotland, 2020 and 2021 (faceted by age group)",
+  ggtitle("Cumulative Excess Deaths in Scotland, 2020 and 2022 (faceted by age group)",
           subtitle = GraphSubtitle)+
   labs(caption = GraphCaption,
        caption.justification = "left")+
