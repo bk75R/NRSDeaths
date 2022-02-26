e079cfeaf1105fe2adb7ed11433125ca1626d33f
bk75R <82215025+bk75R@users.noreply.github.com>
2022-02-26 17:51:26 +0000

    Update NRSDeaths_GraphsCheck.R
    Added faceted version of "NRS Weekly Deaths by Age (2015-2019)".

--- NRSDeaths_GraphsCheck.R
+++ NRSDeaths_GraphsCheck.R
@@ -15,7 +15,11 @@ VlinesYears <- data.frame(Date = c(as.Date("2015-01-01"),
                                    Year = c(2015,2016,2017,2018,2019)
                           )
 
-
+YearLabelPositions2015_2019 <- c(as.Date("2015-03-06"),
+                                 as.Date("2016-03-06"),
+                                 as.Date("2017-03-06"),
+                                 as.Date("2018-03-06"),
+                                 as.Date("2019-03-06"))
 ###################################
 #
 #
@@ -89,7 +93,8 @@ NRSWeeklyDeaths_2015_2019_Graph = ggplot(data = DeathsWeekly_pivot_aggregate_201
   scale_x_date(name = "Date",
                date_breaks = "12 months",
                date_labels = "%Y",
-               minor_breaks = "3 months")+
+               #minor_breaks = "3 months",
+               labels = YearLabelPositions2015_2019)+
   scale_y_continuous(name = "Weekly Deaths",
                      limits = c(NA,NA),
                      labels = label_comma(accuracy = 1))+
@@ -105,7 +110,6 @@ NRSWeeklyDeaths_2015_2019_Graph = ggplot(data = DeathsWeekly_pivot_aggregate_201
             alpha = 1,
             linetype = 1,
             show.legend = TRUE)+
-  
   facet_wrap(vars(Age),
              ncol = 5,
              scales = "free_y")
@@ -130,12 +134,65 @@ ggsave(filename = paste(GraphFileNameRoot,GraphFileName,sep=""),
 #
 ###################################
 
-GraphFileName = " NRS Weekly Deaths by Age (2015-2019, average).png"
+GraphFileName = " NRS Weekly Deaths by Age (2015-2019, average) - faceted.png"
 
 NRSWeeklyDeaths_Average_Graph = ggplot(data = DeathsWeekly_mean_2015_2019,
                                          mapping = aes(x=Week,
                                                        y=Deaths,
-                                                       colour=Age)
+                                                       group=Age)
+)+
+  theme_minimal()+
+  theme(panel.background = element_rect(fill = 'white', color = 'white'),
+        plot.background = element_rect(fill = 'white', color = 'white'),
+        legend.position="bottom",
+        legend.justification = "centre",
+        strip.background = element_blank(),
+        strip.placement = "outside",
+        panel.grid = element_blank(),
+        plot.caption = element_text(hjust = 0))+
+  scale_x_continuous(name = "Week number",
+                     breaks = c(0,10,20,30,40,50))+
+  scale_y_continuous(name = "Weekly Deaths",
+                     limits = c(NA,NA),
+                     labels = label_number(accuracy = NULL))+
+  ggtitle("NRS Weekly Deaths by Age (2015-2019, average)",
+          subtitle = GraphSubtitle)+
+  labs(caption = GraphCaption)+
+  geom_line(size = 0.5,
+            alpha = 1,
+            linetype = 1,
+            #aes(),
+            show.legend = TRUE)+
+  facet_wrap(vars(Age),
+             ncol = 4,
+             scales = "free_y")
+
+#Save graph
+ggsave(filename = paste(GraphFileNameRoot,GraphFileName,sep=""),
+       plot = NRSWeeklyDeaths_Average_Graph,
+       device="png",
+       width=graphwidth,
+       height=graphheight,
+       units="mm",
+       dpi=300
+)
+
+###########################################
+
+###################################
+#
+#
+# Average Weekly deaths by age (All) (Scotland)
+#
+#
+###################################
+
+GraphFileName = " NRS Weekly Deaths by Age (2015-2019, average).png"
+
+NRSWeeklyDeaths_Average_Graph = ggplot(data = DeathsWeekly_mean_2015_2019,
+                                       mapping = aes(x=Week,
+                                                     y=Deaths,
+                                                     colour=Age)
 )+
   theme_bw()+
   scale_x_continuous(name = "Week number")+
@@ -206,7 +263,6 @@ ggsave(filename = paste(GraphFileNameRoot,GraphFileName,sep=""),
 )
 
 ###########################################
-
 
 
 ###################################
