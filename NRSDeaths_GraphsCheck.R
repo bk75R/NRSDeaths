e6e757f4d870e4a02f2939a24d642ab4cda2db44
bk75R <82215025+bk75R@users.noreply.github.com>
2022-02-28 12:37:48 +0000

    Update NRSDeaths_GraphsCheck.R
    Removed "gradient" graphs and graphs that didn't have faceting.

--- NRSDeaths_GraphsCheck.R
+++ NRSDeaths_GraphsCheck.R
@@ -20,50 +20,6 @@ YearLabelPositions2015_2019 <- c(as.Date("2015-03-06"),
                                  as.Date("2017-03-06"),
                                  as.Date("2018-03-06"),
                                  as.Date("2019-03-06"))
-###################################
-#
-#
-# Weekly deaths by age (All) (Scotland)
-#
-#
-###################################
-
-GraphFileName = " NRS Weekly Deaths by Age (2015-2019).png"
-
-NRSWeeklyDeaths_2015_2019_Graph = ggplot(data = DeathsWeekly_pivot_aggregate_2015_2019,#filter(DeathsWeekly_pivot_aggregate_2015_2019,Year == "2019"),
-                                      mapping = aes(x=Date,
-                                                    y=Deaths,
-                                                    colour=Age)
-)+
-  theme_bw()+
-  scale_x_date(name = "Date (MM/YY)",
-               date_breaks = "3 months",
-               date_labels = "%m/%y",
-               minor_breaks = "1 month")+
-  scale_y_continuous(name = "Weekly Deaths",
-                     limits = c(0,NA),
-                     labels = scales::comma
-  )+
-  ggtitle("NRS Weekly Deaths by Age (2015-2019)",
-          subtitle = GraphSubtitle)+
-  labs(caption = GraphCaption)+
-  geom_line(size = 0.5,
-            alpha = 1,
-            linetype = 1,
-            #aes(),
-            show.legend = TRUE)
-
-#Save graph
-ggsave(filename = paste(GraphFileNameRoot,GraphFileName,sep=""),
-       plot = NRSWeeklyDeaths_2015_2019_Graph,
-       device="png",
-       width=graphwidth,
-       height=graphheight,
-       units="mm",
-       dpi=300
-)
-
-###########################################
 
 
 ###################################
@@ -88,33 +44,21 @@ NRSWeeklyDeaths_2015_2019_Graph = ggplot(data = DeathsWeekly_pivot_aggregate_201
         legend.justification = "centre",
         strip.background = element_blank(),
         strip.placement = "outside",
-        # panel.grid = element_blank(),
         plot.caption = element_text(hjust = 0)
         )+
-  scale_x_date(#limits = c(as.Date("2021-04-01"),NA),
-    name = "Date",
-    #date_breaks = "12 months",
-    breaks = c(as.Date("2015-01-01"),
-               as.Date("2016-01-01"),
-               as.Date("2017-01-01"),
-               as.Date("2018-01-01"),
-               as.Date("2019-01-01")),
-    date_labels = "%Y")+
-  # scale_x_date(name = "Date",
-  #              date_breaks = "12 months",
-  #              date_labels = "%Y",
-  #              #minor_breaks = "3 months",
-  #              labels = YearLabelPositions2015_2019)+
+  scale_x_date(name = "Date",
+               breaks = c(as.Date("2015-01-01"),
+                          as.Date("2016-01-01"),
+                          as.Date("2017-01-01"),
+                          as.Date("2018-01-01"),
+                          as.Date("2019-01-01")),
+               date_labels = "%Y")+
   scale_y_continuous(name = "Weekly Deaths",
                      limits = c(NA,NA),
                      labels = label_comma(accuracy = 1))+
   ggtitle("NRS Weekly Deaths by Age (2015-2019)",
           subtitle = GraphSubtitle)+
   labs(caption = GraphCaption)+
-  # geom_vline(data = VlinesYears,
-  #            aes(xintercept = Date),
-  #            colour = "grey75",
-  #            show.legend = FALSE)+
   geom_line(size = 0.25,
             alpha = 1,
             linetype = 1,
@@ -157,7 +101,6 @@ NRSWeeklyDeaths_Average_Graph = ggplot(data = DeathsWeekly_mean_2015_2019,
         legend.justification = "centre",
         strip.background = element_blank(),
         strip.placement = "outside",
-        # panel.grid = element_blank(),
         plot.caption = element_text(hjust = 0)
         )+
   scale_x_continuous(name = "Week number",
@@ -171,7 +114,6 @@ NRSWeeklyDeaths_Average_Graph = ggplot(data = DeathsWeekly_mean_2015_2019,
   geom_line(size = 0.5,
             alpha = 1,
             linetype = 1,
-            #aes(),
             show.legend = TRUE)+
   facet_wrap(vars(Age),
              ncol = 4,
@@ -188,193 +130,8 @@ ggsave(filename = paste(GraphFileNameRoot,GraphFileName,sep=""),
 )
 
 ###########################################
-
-###################################
-#
-#
-# Average Weekly deaths by age (All) (Scotland)
-#
-#
-###################################
-
-GraphFileName = " NRS Weekly Deaths by Age (2015-2019, average).png"
-
-NRSWeeklyDeaths_Average_Graph = ggplot(data = DeathsWeekly_mean_2015_2019,
-                                       mapping = aes(x=Week,
-                                                     y=Deaths,
-                                                     colour=Age)
-)+
-  theme_bw()+
-  scale_x_continuous(name = "Week number")+
-  scale_y_continuous(name = "Weekly Deaths",
-                     limits = c(0,NA),
-                     labels = label_comma(accuracy = NULL))+
-  ggtitle("NRS Weekly Deaths by Age (2015-2019, average)",
-          subtitle = GraphSubtitle)+
-  labs(caption = GraphCaption)+
-  geom_line(size = 0.5,
-            alpha = 1,
-            linetype = 1,
-            #aes(),
-            show.legend = TRUE)
-
-#Save graph
-ggsave(filename = paste(GraphFileNameRoot,GraphFileName,sep=""),
-       plot = NRSWeeklyDeaths_Average_Graph,
-       device="png",
-       width=graphwidth,
-       height=graphheight,
-       units="mm",
-       dpi=300
-)
-
+# Calculate summary data
 ###########################################
-
-###################################
-#
-#
-# Average Weekly deaths by age (baseline for 2020-2022) (Scotland)
-#
-#
-###################################
-
-GraphFileName = " NRS excess deaths by age (baseline for 2020-2022).png"
-
-NRSWeeklyDeaths_Baseline_Graph = ggplot(data = DeathsWeekly_ExcessBaseline,
-                                       mapping = aes(x=Date,
-                                                     y=Deaths,
-                                                     colour=Age)
-)+
-  theme_bw()+
-  scale_x_date(name = "Date (MM/YY)",
-               date_breaks = "3 months",
-               date_labels = "%m/%y",
-               minor_breaks = "1 month")+
-  scale_y_continuous(name = "Weekly Deaths",
-                     limits = c(0,NA),
-                     labels = label_comma(accuracy = NULL))+
-  ggtitle("NRS excess deaths by age (baseline for 2020-2022)",
-          subtitle = GraphSubtitle)+
-  labs(caption = GraphCaption)+
-  geom_line(size = 0.5,
-            alpha = 1,
-            linetype = 1,
-            #aes(),
-            show.legend = TRUE)
-
-#Save graph
-ggsave(filename = paste(GraphFileNameRoot,GraphFileName,sep=""),
-       plot = NRSWeeklyDeaths_Baseline_Graph,
-       device="png",
-       width=graphwidth,
-       height=graphheight,
-       units="mm",
-       dpi=300
-)
-
-###########################################
-
-
-###################################
-#
-#
-# DeathsTogether
-#
-#
-###################################
-
-GraphFileName = " DeathsTogether.png"
-
-NRSWeeklyDeaths_Excess_Graph = ggplot(data = DeathsTogether,
-                                        mapping = aes(x=Date,
-                                                      y=Deaths,
-                                                      colour=Age,
-                                                      linetype = Cause)
-)+
-  theme_bw()+
-  scale_x_date(limits = c(as.Date("2020-12-01"),NA),
-    name = "Date (MM/YY)",
-               date_breaks = "1 week",
-               date_labels = "%d/%m/%y"#,
-               #minor_breaks = "1 month"
-               )+
-  scale_y_continuous(name = "Weekly Deaths",
-                     limits = c(0,NA),
-                     labels = label_number(accuracy = NULL))+
-  ggtitle("DeathsTogether",
-          subtitle = GraphSubtitle)+
-  labs(caption = GraphCaption)+
-  geom_line(size = 0.5,
-            alpha = 1,
-            #linetype = 1,
-            #aes(),
-            show.legend = TRUE)
-
-#Save graph
-ggsave(filename = paste(GraphFileNameRoot,GraphFileName,sep=""),
-       plot = NRSWeeklyDeaths_Excess_Graph,
-       device="png",
-       width=graphwidth,
-       height=graphheight,
-       units="mm",
-       dpi=300
-)
-
-###########################################
-
-###################################
-#
-#
-# DeathsTogether (faceted)
-#
-#
-###################################
-
-GraphFileName = " Deaths - All Causes and Expected (faceted by age group).png"
-
-NRSWeeklyDeaths_Excess_Graph = ggplot(data = filter(DeathsTogether,Date >= as.Date("2021-04-01")),
-                                      mapping = aes(x=Date,
-                                                    y=Deaths,
-                                                    #colour=Age,
-                                                    linetype = Cause)
-)+
-  theme_minimal()+
-  theme(panel.background = element_rect(fill = 'white', color = 'white'),
-        plot.background = element_rect(fill = 'white', color = 'white'),
-        legend.position="bottom",
-        legend.justification = "centre",
-        strip.background = element_blank(),
-        strip.placement = "outside")+
-  scale_x_date(#limits = c(as.Date("2021-04-01"),NA),
-               name = "Month",
-               date_breaks = "1 month",
-               date_labels = "%b"
-  )+
-  scale_y_continuous(name = "Weekly Deaths",
-                     labels = scales::comma)+
-  ggtitle("Deaths - All Causes and Expected (faceted by age group)",
-          subtitle = GraphSubtitle)+
-  labs(caption = GraphCaption,
-       caption.justification = "left")+
-  geom_line(size = 0.5,
-            alpha = 1,
-            show.legend = TRUE)+
-  facet_wrap(vars(Age),
-             ncol = 4,
-             scales = "free_y")+
-  scale_linetype_manual(name = "Type of Death",
-                        values = ScaleDeathLinetypes,
-                        labels = LineLabels)
-
-#Save graph
-ggsave(filename = paste(GraphFileNameRoot,GraphFileName,sep=""),
-       plot = NRSWeeklyDeaths_Excess_Graph,
-       device="png",
-       width=graphwidth,
-       height=graphheight,
-       units="mm",
-       dpi=300
-)
 
 DeathsTogetherSummary <- DeathsTogether %>% 
   filter(Date >= as.Date("2021-04-01")) %>%
@@ -388,78 +145,7 @@ DeathsTogetherSummary <- DeathsTogether %>%
 DeathsTogetherSummarySum = sum(DeathsTogetherSummary$Difference)
 
            
-           
-
-###########################################
-
-
-###################################
-#
-#
-# DeathsTogether (faceted) with loess smoothed fits
-#
-#
-###################################
-
-GraphFileName = " Deaths - All Causes and Expected with fit lines (faceted by age group).png"
-
-NRSWeeklyDeaths_Excess_Graph = ggplot(data = filter(DeathsTogether,Date >= as.Date("2021-04-01")),
-                                      mapping = aes(x=Date,
-                                                    y=Deaths,
-                                                    #colour=Age,
-                                                    shape = Cause,
-                                                    linetype = Cause)
-)+
-  theme_minimal()+
-  theme(panel.background = element_rect(fill = 'white', color = 'white'),
-        plot.background = element_rect(fill = 'white', color = 'white'),
-        legend.position="bottom",
-        legend.justification = "centre",
-        strip.background = element_blank(),
-        strip.placement = "outside")+
-  scale_x_date(#limits = c(as.Date("2021-04-01"),NA),
-    name = "Month",
-    date_breaks = "1 month",
-    date_labels = "%b"
-  )+
-  scale_y_continuous(name = "Weekly Deaths",
-                     labels = scales::comma)+
-  ggtitle("Deaths - All Causes and Expected with Fit Lines (faceted by age group)",
-          subtitle = GraphSubtitle)+
-  labs(caption = GraphCaption,
-       caption.justification = "left")+
-  # geom_line(size = 0.5,
-  #           alpha = 1,
-  #           show.legend = TRUE)+
-  geom_point(size = 1,
-             colour = "black",
-             fill = "black")+
-  geom_smooth(se = FALSE,
-              span = 0.5,
-              size = 0.5,
-              colour = "black")+
-  facet_wrap(vars(Age),
-             ncol = 4,
-             scales = "free_y")+
-  scale_linetype_manual(name = "Type of Death",
-                        values = ScaleDeathLinetypes,
-                        labels = LineLabels)+
-  scale_shape_manual(name = "Type of Death",
-                     values = ScaleDeathPointTypes,
-                     labels = LineLabels)
-
-#Save graph
-ggsave(filename = paste(GraphFileNameRoot,GraphFileName,sep=""),
-       plot = NRSWeeklyDeaths_Excess_Graph,
-       device="png",
-       width=graphwidth,
-       height=graphheight,
-       units="mm",
-       dpi=300
-)
-
-
-###########################################
+##########################################
 
 ###################################
 #
@@ -468,7 +154,6 @@ ggsave(filename = paste(GraphFileNameRoot,GraphFileName,sep=""),
 #
 #
 ###################################
-#graphheight = 2*graphheight
 
 GraphFileName = " Excess Deaths in Scotland (2020-2022) - faceted.png"
 
@@ -495,7 +180,7 @@ NRSWeeklyDeaths_Excess_Graph = ggplot(data = filter(DeathsTogether,Cause == "All
   scale_y_continuous(name = "Weekly Deaths",
                      #limits = c(-25,100),
                      labels = label_number(accuracy = NULL))+
-  ggtitle("Excess Deaths in Scotland (2020-2022)",
+  ggtitle("Excess Deaths in Scotland (2020-2022) using 2015-2019 average deaths as baseline",
           subtitle = GraphSubtitle)+
   labs(caption = GraphCaption)+
   geom_line(size = 0.5,
@@ -517,57 +202,6 @@ ggsave(filename = paste(GraphFileNameRoot,GraphFileName,sep=""),
        dpi=300
 )
 
-#graphheight = 0.5*graphheight
-###########################################
-
-###################################
-#
-#
-# ExcessDeaths
-#
-#
-###################################
-#graphheight = 2*graphheight
-
-GraphFileName = " Excess Deaths in Scotland (2020-2022).png"
-
-NRSWeeklyDeaths_Excess_Graph = ggplot(data = filter(DeathsTogether,Cause == "All"),
-                                      mapping = aes(x=Date,
-                                                    y=Excess,
-                                                    colour=Age)
-)+
-  theme_bw()+
-  scale_x_date(#limits = c(as.Date("2020-12-01"),NA),
-               name = "Date (MM/YY)",
-               date_breaks = "1 month",
-               date_labels = "%m/%y"
-  )+
-  scale_y_continuous(name = "Weekly Deaths",
-                     limits = c(NA,NA),
-                     labels = label_number(accuracy = NULL))+
-  ggtitle("Excess Deaths in Scotland (2020-2022)",
-          subtitle = GraphSubtitle)+
-  labs(caption = GraphCaption)+
-  geom_line(size = 0.5,
-            alpha = 1,
-            #linetype = 1,
-            #aes(),
-            show.legend = TRUE)#+
-  # facet_wrap(vars(Age),
-  #          ncol = 4,
-  #          scales = "fixed")
-
-#Save graph
-ggsave(filename = paste(GraphFileNameRoot,GraphFileName,sep=""),
-       plot = NRSWeeklyDeaths_Excess_Graph,
-       device="png",
-       width=graphwidth,
-       height=graphheight,
-       units="mm",
-       dpi=300
-)
-
-#graphheight = 0.5*graphheight
 ###########################################
 
 setwd(RootDirectory)
